b0a22c99ef6f58fadc4cf2c333f8dae5d3fd8d37
Hai Phan Nguyen <pnghai@gmail.com>
2014-10-18 16:21:07 +0700

    add logic for checking email existed

--- assets/coffee/cems-event17.coffee
+++ assets/coffee/cems-event17.coffee
@@ -10,25 +10,34 @@ jQuery.noConflict()
 jQuery(document).ready ($) ->
 
   # Sending something...
-  CEMSAjaxCall = (submitBtn, cems_action, form) ->
+  CEMSAjaxCall = (submitBtn, cems_action, form, alert_box) ->
     submitBtn.bootstrapBtn "loading"
-    $alert = $("#cems-alert")
+    $alert = $(alert_box)
     $alert.fadeOut "slow"
 
-    # OK
-
-    # Error
     $.post(wpdk_i18n.ajaxURL, "action=" + cems_action + "&" + $(form).serialize(), (result) ->
       response = new WPDKAjaxResponse(result)
       textResponse = ""
+      #TODO: need to refactor below code
       if empty(response.error)
-        textResponse = response.message + "<br>" + response.data
+        if cems_action is "get_customer_existed_action"
+          textResponse = response.message
+          #setup data here
+          data = $.parseJSON(response.data)
+          $(form).find('#customer-email').prop('readOnly',true)
+          $(form).find('#customer-fullname').val(data.full_name).prop('readOnly',true)
+          $(form).find('#customer-phone').val(data.phone).prop('readOnly',true)
+          $(form).find('#customer-birthday').val(moment(data.birthday, 'YYYY-MM-DD').format('DD-MM-YYYY')).prop('readOnly',true)
+        else
+          textResponse = response.message + "<br>" + response.data
         $alert.removeClass("alert-danger").addClass "alert-success"
       else
         textResponse = response.error
         $alert.removeClass("alert-success").addClass "alert-danger"
       $alert.children(".error-response").html textResponse
       $alert.fadeIn "slow"
+      $.scrollTo $alert, 800,
+        offset:-50
       return
     ).always ->
       submitBtn.bootstrapBtn "reset"
@@ -54,8 +63,10 @@ jQuery(document).ready ($) ->
 
     e.preventDefault()
     $form = $(e.target)
-    CEMSAjaxCall $form.find('[type=submit]:not(.bv-hidden-submit)'), "register_new_event_action", $form
+    CEMSAjaxCall $form.find('[type=submit]:not(.bv-hidden-submit)'), "register_new_event_action", $form, '#cems-alert'
     return
+
+  #birthday validation
   $("#customer-birthday").datepicker(
     autoclose:"true"
   ).on "changeDate show", (e) ->
@@ -63,4 +74,21 @@ jQuery(document).ready ($) ->
     $("#event17-form").bootstrapValidator "revalidateField", "customer_birthday"
     return
 
+  $("#event17-form .btn-check-exist").click ->
+    $form = $("#event17-form")
+    bootstrapValidator = $form.data('bootstrapValidator')
+    unless bootstrapValidator.isValidField("customer_email")
+      bootstrapValidator.revalidateField("customer_email")
+      return
+
+    CEMSAjaxCall $form.find('.btn-check-exist'), "get_customer_existed_action", $form, "#cems-notify-customer"
+
+  $("#event17-form .btn-reset").click (e)->
+    e.preventDefault()
+    $form = $("#event17-form")
+    $form.find('.alert').hide()
+    $form[0].reset();
+    $form.find('input').prop('readOnly',false)
+    $form.data('bootstrapValidator').resetForm(true)
+    return
   return
--- assets/js/cems-event17.js
+++ assets/js/cems-event17.js
@@ -9,17 +9,26 @@
 
   jQuery(document).ready(function($) {
     var CEMSAjaxCall;
-    CEMSAjaxCall = function(submitBtn, cems_action, form) {
+    CEMSAjaxCall = function(submitBtn, cems_action, form, alert_box) {
       var $alert;
       submitBtn.bootstrapBtn("loading");
-      $alert = $("#cems-alert");
+      $alert = $(alert_box);
       $alert.fadeOut("slow");
       $.post(wpdk_i18n.ajaxURL, "action=" + cems_action + "&" + $(form).serialize(), function(result) {
-        var response, textResponse;
+        var data, response, textResponse;
         response = new WPDKAjaxResponse(result);
         textResponse = "";
         if (empty(response.error)) {
-          textResponse = response.message + "<br>" + response.data;
+          if (cems_action === "get_customer_existed_action") {
+            textResponse = response.message;
+            data = $.parseJSON(response.data);
+            $(form).find('#customer-email').prop('readOnly', true);
+            $(form).find('#customer-fullname').val(data.full_name).prop('readOnly', true);
+            $(form).find('#customer-phone').val(data.phone).prop('readOnly', true);
+            $(form).find('#customer-birthday').val(moment(data.birthday, 'YYYY-MM-DD').format('DD-MM-YYYY')).prop('readOnly', true);
+          } else {
+            textResponse = response.message + "<br>" + response.data;
+          }
           $alert.removeClass("alert-danger").addClass("alert-success");
         } else {
           textResponse = response.error;
@@ -27,6 +36,9 @@
         }
         $alert.children(".error-response").html(textResponse);
         $alert.fadeIn("slow");
+        $.scrollTo($alert, 800, {
+          offset: -50
+        });
       }).always(function() {
         submitBtn.bootstrapBtn("reset");
       });
@@ -55,12 +67,31 @@
       var $form;
       e.preventDefault();
       $form = $(e.target);
-      CEMSAjaxCall($form.find('[type=submit]:not(.bv-hidden-submit)'), "register_new_event_action", $form);
+      CEMSAjaxCall($form.find('[type=submit]:not(.bv-hidden-submit)'), "register_new_event_action", $form, '#cems-alert');
     });
     $("#customer-birthday").datepicker({
       autoclose: "true"
     }).on("changeDate show", function(e) {
       $("#event17-form").bootstrapValidator("revalidateField", "customer_birthday");
+    });
+    $("#event17-form .btn-check-exist").click(function() {
+      var $form, bootstrapValidator;
+      $form = $("#event17-form");
+      bootstrapValidator = $form.data('bootstrapValidator');
+      if (!bootstrapValidator.isValidField("customer_email")) {
+        bootstrapValidator.revalidateField("customer_email");
+        return;
+      }
+      return CEMSAjaxCall($form.find('.btn-check-exist'), "get_customer_existed_action", $form, "#cems-notify-customer");
+    });
+    $("#event17-form .btn-reset").click(function(e) {
+      var $form;
+      e.preventDefault();
+      $form = $("#event17-form");
+      $form.find('.alert').hide();
+      $form[0].reset();
+      $form.find('input').prop('readOnly', false);
+      $form.data('bootstrapValidator').resetForm(true);
     });
   });
 
