fc8bd97e0335611d1369ca315975b34185fa5555
Hai Phan Nguyen <pnghai@gmail.com>
2014-12-10 11:32:32 +0700

    fix form

--- classes/core/cems-event17.php
+++ classes/core/cems-event17.php
@@ -73,95 +73,92 @@ class CEMSEvent17 {
                     <input type="hidden" name="event_register[customer_source]">
 
                     <div class="form-group">
-                        <label for="customer-email" class="col-sm-3 control-label"><?php echo __('
-Địa chỉ email',WPCEMS_TEXTDOMAIN);?><sup>*</sup><br>Bạn nhớ click Kiểm tra trước khi điền Họ tên</label>
-                        <div class="col-sm-5">
-                            <input type="email" class="form-control" id="customer-email" name="customer[email]" placeholder="<?php echo __('Nhập địa chỉ email của bạn',WPCEMS_TEXTDOMAIN);?>" data-bv-notempty="true"
-                                   data-bv-notempty-message="Bạn cần điền email"
+                        <label for="customer-email" class="control-label"><?php echo __('
+Địa chỉ email',WPCEMS_TEXTDOMAIN);?> <sup>*</sup> (Bạn nhớ click Kiểm tra trước khi điền Họ tên)</label>
+                        <div class="row">
+                            <div class="col-xs-10">
+                                <input type="email" class="form-control" id="customer-email" name="customer[email]" placeholder="<?php echo __('Nhập địa chỉ email của bạn',WPCEMS_TEXTDOMAIN);?>" data-bv-notempty="true"
+                                       data-bv-notempty-message="Bạn cần điền email"
 
-                                   data-bv-emailaddress="true"
-                                   data-bv-emailaddress-message="Email không hợp lệ">
-                        </div>
-                        <div class="col-sm-3">
-                            <button type="button" class="btn btn-warning btn-check-exist" data-loading-text="Xin chờ ..." data-toggle='tooltip' data-placement='bottom' title='Nếu bạn đã từng đăng ký với chúng tôi, bạn chỉ cần điền email, những thông tin khác sẽ được tự động điền giúp bạn.'>Kiểm tra</button>
+                                       data-bv-emailaddress="true"
+                                       data-bv-emailaddress-message="Email không hợp lệ">
+                            </div>
+                            <div class="col-xs-2">
+                                <button type="button" class="btn btn-warning btn-block btn-check-exist" data-loading-text="Xin chờ ..." data-toggle="tooltip" data-placement="bottom" title="Nếu bạn đã từng đăng ký với chúng tôi, bạn chỉ cần điền email, những thông tin khác sẽ được tự động điền giúp bạn.">Kiểm tra</button>
+                            </div>
                         </div>
                     </div>
-                    <div class="alert alert-danger cems-notify-customer" role="alert" style="display: none">
+
+                    <div class="alert alert-danger cems-notify-customer" role="alert" id="cems-notify-customer" style="display: none">
                         <div class="error-response"></div>
                     </div>
                     <div class="form-group">
-                        <label for="customer-fullname" class="col-sm-3 control-label"><?php echo __('
+
+                        <label for="customer-fullname" class="control-label"><?php echo __('
 Họ Tên',WPCEMS_TEXTDOMAIN);?><sup>*</sup></label>
-                        <div class="col-sm-8">
-                            <input type="text" class="form-control" id="customer-fullname" name="customer[full_name]" placeholder="<?php echo __('Nhập họ tên',WPCEMS_TEXTDOMAIN);?>" data-bv-notempty="true"
-                                   data-bv-notempty-message="Họ tên không được để trống"
+                        <input type="text" class="form-control" id="customer-fullname" name="customer[full_name]" placeholder="<?php echo __('Nhập họ tên',WPCEMS_TEXTDOMAIN);?>" data-bv-notempty="true"
+                               data-bv-notempty-message="Họ tên không được để trống"
 
-                                   data-bv-stringlength="true"
-                                   data-bv-stringlength-min="3"
-                                   data-bv-stringlength-max="255"
-                                   data-bv-stringlength-message="Họ tên chỉ dài 3-255 ký tự">
-                        </div>
+                               data-bv-stringlength="true"
+                               data-bv-stringlength-min="3"
+                               data-bv-stringlength-max="255"
+                               data-bv-stringlength-message="Họ tên chỉ dài 3-255 ký tự">
                     </div>
 
                     <div class="form-group">
-                        <label for="customer-phone" class="col-sm-3 control-label"><?php echo __('
-Số điện thoại',WPCEMS_TEXTDOMAIN);?><sup>*</sup></label>
-                        <div class="col-sm-8">
-                            <input type="tel" class="form-control" id="customer-phone" name="customer[phone]" placeholder="<?php echo __('Nhập số điện thoại của bạn',WPCEMS_TEXTDOMAIN);?>" data-bv-notempty="true"
-                                   data-bv-notempty-message="Không được để trống số điện thoại"
+                        <label for="customer-phone" class="control-label"><?php echo __('
+Số điện thoại',WPCEMS_TEXTDOMAIN);?> <sup>*</sup></label>
+                        <input type="tel" class="form-control" id="customer-phone" name="customer[phone]" placeholder="<?php echo __('Nhập số điện thoại của bạn',WPCEMS_TEXTDOMAIN);?>" data-bv-notempty="true"
+                               data-bv-notempty-message="Không được để trống số điện thoại"
 
-                                   data-bv-stringlength="true"
-                                   data-bv-stringlength-min="6"
-                                   data-bv-stringlength-max="15"
-                                   data-bv-stringlength-message="Số điện thoại chỉ nằm trong 6-15 ký số"
+                               data-bv-stringlength="true"
+                               data-bv-stringlength-min="6"
+                               data-bv-stringlength-max="15"
+                               data-bv-stringlength-message="Số điện thoại chỉ nằm trong 6-15 ký số"
 
-                                   data-bv-regexp="true"
-                                   data-bv-regexp-regexp="^[0-9]+$"
-                                   data-bv-regexp-message="Số điện thoại chỉ gồm một chuỗi các số">
-                        </div>
+                               data-bv-regexp="true"
+                               data-bv-regexp-regexp="^[0-9]+$"
+                               data-bv-regexp-message="Số điện thoại chỉ gồm một chuỗi các số">
                     </div>
 
-                    <div class="form-group">
-                        <label for="customer-birthday" class="col-sm-3 control-label">Ngày sinh<sup>*</sup><br>Sử dụng dấu cách ( - ): 20-10-1980</label>
-                        <div class="col-sm-8">
-                            <input type="text" class="form-control" id="customer-birthday" name="customer[birthday]" placeholder="DD-MM-YYYY"
-                                   data-bv-notempty="true"
-                                   data-bv-notempty-message="Bạn phải điền ngày sinh"
 
-                                   data-bv-date="true"
-                                   data-bv-date-format="DD-MM-YYYY"
-                                   data-bv-date-message="Ngày sinh không hợp lệ"
-                                   data-provide="datepicker"
-                                   data-date-format="dd-mm-yyyy"
-                                   data-date-language="vi"/>
-                        </div>
+                    <div class="form-group">
+                        <label for="customer-birthday" class="control-label">Sinh nhật <sup>*</sup> (Sử dụng dấu cách "-": 20-10-1980)</label>
+                        <input type="text" class="form-control" id="customer-birthday" name="customer[birthday]" placeholder="DD-MM-YYYY"
+                               data-bv-date="true"
+                               data-bv-notempty="true"
+                               data-bv-notempty-message="Không được để trống ngày sinh"
+                               data-bv-date-format="DD-MM-YYYY"
+                               data-bv-date-message="Ngày sinh không hợp lệ"
+                               data-provide="datepicker"
+                               data-date-format="dd-mm-yyyy"
+                               data-date-language="vi"/>
                     </div>
 
+
                     <div class="form-group">
-                        <label class="col-sm-3 control-label">Vì sao bạn biết đến buổi chia sẻ này<sup>*</sup></label>
-                        <div class="col-sm-8">
-                            <div class="radio">
-                                <label>
-                                    <input type="radio" name="event_register[why_you_know]" value="Facebook Life Coaching Vietnam"
-                                           data-bv-notempty="true"
-                                           data-bv-notempty-message="Bạn chưa chọn nguồn thông tin" /> Facebook Life Coaching Vietnam
-                                </label>
-                            </div>
-                            <div class="radio">
-                                <label>
-                                    <input type="radio" name="event_register[why_you_know]" value="Facebook bạn bè người thân chia sẻ" /> Facebook bạn bè người thân chia sẻ
-                                </label>
-                            </div>
-                            <div class="radio">
-                                <label>
-                                    <input type="radio" name="event_register[why_you_know]" value="Truyền miệng từ bạn bè người thân" /> Truyền miệng từ bạn bè người thân
-                                </label>
-                            </div>
-                            <div class="radio">
-                                <label>
-                                    <input type="radio" name="event_register[why_you_know]" value="Trang web lifecoach.com.vn" /> Trang web lifecoach.com.vn
-                                </label>
-                            </div>
+                        <label class="control-label">Vì sao bạn biết đến buổi chia sẻ này &nbsp;<span><sup>*</sup></span></label>
+                        <div class="radio">
+                            <label>
+                                <input type="radio" name="event_register[why_you_know]" value="Facebook Life Coaching Vietnam"
+                                       data-bv-notempty="true"
+                                       data-bv-notempty-message="Bạn chưa chọn nguồn thông tin" /> Facebook Life Coaching Vietnam
+                            </label>
+                        </div>
+                        <div class="radio">
+                            <label>
+                                <input type="radio" name="event_register[why_you_know]" value="Facebook bạn bè người thân chia sẻ" /> Facebook bạn bè người thân chia sẻ
+                            </label>
+                        </div>
+                        <div class="radio">
+                            <label>
+                                <input type="radio" name="event_register[why_you_know]" value="Truyền miệng từ bạn bè người thân" /> Truyền miệng từ bạn bè người thân
+                            </label>
+                        </div>
+                        <div class="radio">
+                            <label>
+                                <input type="radio" name="event_register[why_you_know]" value="Trang web lifecoach.com.vn" /> Trang web lifecoach.com.vn
+                            </label>
                         </div>
                     </div>
 
